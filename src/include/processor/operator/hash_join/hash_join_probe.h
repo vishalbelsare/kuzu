a05333c062a3a35db2fb91d628c07101393f8034
xiyang <x74feng@uwaterloo.ca>
2023-04-13 00:14:36 -0400

    rewrok hashjoin

--- src/include/processor/operator/hash_join/hash_join_probe.h
+++ src/include/processor/operator/hash_join/hash_join_probe.h
@@ -47,46 +47,61 @@ public:
 };
 
 // Probe side on left, i.e. children[0] and build side on right, i.e. children[1]
-class HashJoinProbe : public PhysicalOperator, SelVectorOverWriter {
+class HashJoinProbe : public PhysicalOperator, public SelVectorOverWriter {
 public:
     HashJoinProbe(std::shared_ptr<HashJoinSharedState> sharedState, common::JoinType joinType,
-        const ProbeDataInfo& probeDataInfo, std::unique_ptr<PhysicalOperator> probeChild,
-        std::unique_ptr<PhysicalOperator> buildChild, uint32_t id, const std::string& paramsString)
+        bool singletonProbe, const ProbeDataInfo& probeDataInfo,
+        std::unique_ptr<PhysicalOperator> probeChild, std::unique_ptr<PhysicalOperator> buildChild,
+        uint32_t id, const std::string& paramsString)
         : PhysicalOperator{PhysicalOperatorType::HASH_JOIN_PROBE, std::move(probeChild),
               std::move(buildChild), id, paramsString},
-          sharedState{std::move(sharedState)}, joinType{joinType}, probeDataInfo{probeDataInfo} {}
+          sharedState{std::move(sharedState)}, joinType{joinType}, singletonProbe{singletonProbe},
+          probeDataInfo{probeDataInfo} {}
 
     // This constructor is used for cloning only.
     // HashJoinProbe do not need to clone hashJoinBuild which is on a different pipeline.
     HashJoinProbe(std::shared_ptr<HashJoinSharedState> sharedState, common::JoinType joinType,
-        const ProbeDataInfo& probeDataInfo, std::unique_ptr<PhysicalOperator> probeChild,
-        uint32_t id, const std::string& paramsString)
+        bool singletonProbe, const ProbeDataInfo& probeDataInfo,
+        std::unique_ptr<PhysicalOperator> probeChild, uint32_t id, const std::string& paramsString)
         : PhysicalOperator{PhysicalOperatorType::HASH_JOIN_PROBE, std::move(probeChild), id,
               paramsString},
-          sharedState{std::move(sharedState)}, joinType{joinType}, probeDataInfo{probeDataInfo} {}
+          sharedState{std::move(sharedState)}, joinType{joinType}, singletonProbe{singletonProbe},
+          probeDataInfo{probeDataInfo} {}
 
     void initLocalStateInternal(ResultSet* resultSet, ExecutionContext* context) override;
 
     bool getNextTuplesInternal(ExecutionContext* context) override;
 
     inline std::unique_ptr<PhysicalOperator> clone() override {
-        return make_unique<HashJoinProbe>(
-            sharedState, joinType, probeDataInfo, children[0]->clone(), id, paramsString);
+        return make_unique<HashJoinProbe>(sharedState, joinType, singletonProbe, probeDataInfo,
+            children[0]->clone(), id, paramsString);
     }
 
-private:
-    bool hasMoreLeft();
-    bool getNextBatchOfMatchedTuples(ExecutionContext* context);
-    uint64_t getNextInnerJoinResult();
-    uint64_t getNextLeftJoinResult();
-    uint64_t getNextMarkJoinResult();
+protected:
+    inline bool getMatchedTuples(ExecutionContext* context) {
+        return singletonProbe ? getMatchedTuplesForSingletonInput(context) :
+                                getMatchedTuplesForBatchedInput(context);
+    }
+    bool getMatchedTuplesForSingletonInput(ExecutionContext* context);
+    // We can probe a batch of input tuples if we know they have at most one match.
+    bool getMatchedTuplesForBatchedInput(ExecutionContext* context);
+
+    inline uint64_t getInnerJoinResult() {
+        return singletonProbe ? getInnerJoinResultForSingletonInput() :
+                                getInnerJoinResultForBatchedInput();
+    }
+    uint64_t getInnerJoinResultForSingletonInput();
+    uint64_t getInnerJoinResultForBatchedInput();
+    uint64_t getLeftJoinResult();
+    uint64_t getMarkJoinResult();
+    uint64_t getJoinResult();
+
     void setVectorsToNull();
 
-    uint64_t getNextJoinResult();
-
-private:
+protected:
     std::shared_ptr<HashJoinSharedState> sharedState;
     common::JoinType joinType;
+    bool singletonProbe;
 
     ProbeDataInfo probeDataInfo;
     std::vector<common::ValueVector*> vectorsToReadInto;
